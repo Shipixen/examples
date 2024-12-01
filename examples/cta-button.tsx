a3713fcd0e61fec1ec3823d0eae07eab8c2c9f0a
Dan Mindru <1515742+danmindru@users.noreply.github.com>
2024-12-01 12:19:32 +0100

    Create cta-button.tsx

---
+++ examples/cta-button.tsx
@@ -0,0 +1,9 @@
+import { CtaButton } from '@/components/shared/ui/cta-button';
+
+export default function Component() {
+  return (
+    <div className="flex">
+      <CtaButton>Get started</CtaButton>
+    </div>
+  );
+}
